cdf2eca6c32890389b18073b270b6403c01fe95b
Anton Lin <antonjlin@gmail.com>
2019-01-26 00:53:11 -0800

    update websocket

--- dashboard/test.js
+++ dashboard/test.js
@@ -7,10 +7,60 @@ logger.config({
 })
 
 let client
-
 client = new WebSocket('ws://192.168.49.1:8887')
-
 logger.success(`Connected to websocket server: ${client.url}`)
+
+/*
+All messages will be in this sample format:
+
+{
+    "event": "pid/telemetry/whatever",
+    "ts": 1548488635655,
+    "body":{ //whatever stuff the event entails }
+}
+
+EX:
+{
+    "event": "ping",
+    "ts": 1548488635655,
+    "body":{}
+}
+
+{
+    "event": "pid",
+    "ts": 1548488635655,
+    "body":{
+        "ts":1548488635655,
+        "error":-10,
+        "de":0,
+        "dt":-0.006,
+        "p":-0.1,
+        "i":-0.115759,
+        "d":0,
+        "output":-0.215759
+    }
+}
+
+// read the telemetry
+{
+    "event": "telemetry",
+    "ts": 1548488635655,
+    "body":{
+        "variable": "heading",
+        "value": 123.234
+    }
+}
+// set settings
+{
+    "event": "settings",
+    "ts": 1548488635655,
+    "body":{
+        "variable": "heading",
+        "value": 123.234
+    }
+}
+
+*/
 
 client.on('message', (message) => {
   try {
